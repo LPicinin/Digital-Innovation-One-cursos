2cd93227bcaa8be62a214ac7edb53cb3fb733dec
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-07-29 22:05:16 -0300

    rotas criadas
    Symfony

---
+++ PHP/php_avançado/cadastro-symfony/src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//https://symfony.com/doc/current/routing.html
+class DefaultController
+{
+    /*
+    public function index(Request $request): Response
+    {
+        $res = new Response();
+        $res->setContent(json_encode([
+            "recebido" => $request->get('nome'),
+            "ip" => $request->getClientIp()
+        ]));
+        $res->setStatusCode(200);
+        return $res;
+        //return new Response("Digital", 200);
+    }
+    */
+}
