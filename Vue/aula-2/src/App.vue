d81b9ee9fdfa2cd57c9b1329813f3ba94f157f80
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-08-05 22:01:36 -0300

    Aula 2 - Maneiras diferentes de criar componentes

---
+++ Vue/aula-2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <ButtonVue></ButtonVue>
+    <ButtonString></ButtonString>
+    <ButtonTemplateString></ButtonTemplateString>
+    <ButtonXTemplate></ButtonXTemplate>
+    <ButtonInLine inline-template>
+      <button @click="click">Click ButtonInLine {{ count }} times</button>
+    </ButtonInLine>
+    <ButtonRender></ButtonRender>
+    <ButtonJSX></ButtonJSX>
+  </div>
+</template>
+
+<script>
+import ButtonVue from "./components/ButtonVue.vue";
+
+export default {
+  name: "App",
+  components: {
+    ButtonVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
