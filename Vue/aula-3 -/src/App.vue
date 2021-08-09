2bda40d8369221b9f512fd2d45ad377eee34a595
LPicinin <luishpj@outlook.com.br>
2021-08-08 23:27:20 -0300

    aula 3 - methods, compute e mount

---
+++ Vue/aula-3 -/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <button @click="increment">Click ButtonInLine {{ count }} times</button>
+    {{ y }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  mounted() {
+    //this.$set(this.count, "y", 1);
+    //this.count = { ...this.count, ...{ a: 1, b: 2 } };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  //parecidas commetodos, porem é um sistema baseado em cache
+  computed:{
+
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
