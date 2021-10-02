af4bf2687be478e1e38fbef2b750de744cad0089
LPicinin <luishpj@outlook.com.br>
2021-10-01 22:27:33 -0300

    Update JediController.java

--- Java/spring-web-mvc/src/main/java/com/globallabs/springmvc/controller/JediController.java
+++ Java/spring-web-mvc/src/main/java/com/globallabs/springmvc/controller/JediController.java
@@ -2,13 +2,15 @@ package com.globallabs.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JediController {
 	
 	@GetMapping("/jedi")
-	public String getJedi()
+	public ModelAndView getJedi()
 	{
-		return "jedi";
+		return new ModelAndView("jedi.html");
 	}
 }
